7b8770a81fb5c54fdf14c20adbea7bf080e2eb6b
carlhaoyu <119887587@qq.com>
2018-07-22 17:00:44 +0800

    下午 5点

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -12,23 +12,44 @@ class ViewController: UIViewController {
         let english=["A","B","C"]
         var ints:[Int]=[1,2,3]
         var floats:Array<Float>=Array( repeating:0,count:5)
-        for index in 0..<3{
-            print("\(english[index])")
-        }
-        for str in english{
-            print(str)
-        }
-        let 吧=[1:"A",2:"C",3:"8"]
-        for( a , b ) in 吧{
+        ints.sort()
+        
+//        for index in 0..<3{
+//            print("\(english[index])")
+//        }
+//        for str in english{
+//            print(str)
+//        }
+        let dic=[1:"A",2:"C",3:"8"]
+        for( a , b ) in dic{
             print("\(a):\(b)")
         }
+    let dicSorted=dic.sorted( by:{ $0.value < $1.value} )
+        
+        
+        for( a , b ) in dicSorted{
+        print("\(a):\(b)")
+        }
+        var times:Int=0
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats:true, block: {
+            smalltimer in
+            times=times+1
+            print("timer:\(smalltimer.isValid)")
+            
+        })
+       _ = self.add(first:1,second:2)
+   
+    }
+
+    func add(first:Int,second:Int)->Int{
+        return first+second
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
-
-
 }
 
