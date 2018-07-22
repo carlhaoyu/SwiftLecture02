8dcaccc472b44e33666747aac4246f648b3618cd
carlhaoyu <119887587@qq.com>
2018-07-22 10:25:58 +0800

    no.1

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -1,19 +1,14 @@
-//
-//  ViewController.swift
-//  SwiftLecture02
-//
-//  Created by stu1 on 2018/7/21.
-//  Copyright © 2018年 stripim. All rights reserved.
-//
 
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         print("Hello,Swift!")
+       var name:String="Liddle"
+        let fullName=name+"liu"
+        print(fullName)
     }
 
     override func didReceiveMemoryWarning() {
