d583066e6adf5369085afa95f01b7ca99d2b45f3
carlhaoyu <119887587@qq.com>
2018-07-22 15:54:17 +0800

    111

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -1,14 +1,27 @@
 
 import UIKit
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbName:UILabel?
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("Hello,Swift!")
        var name:String="Liddle"
         let fullName=name+"liu"
-        print(fullName)
+        lbName?.text=fullName
+        let english=["A","B","C"]
+        var ints:[Int]=[1,2,3]
+        var floats:Array<Float>=Array( repeating:0,count:5)
+        for index in 0..<3{
+            print("\(english[index])")
+        }
+        for str in english{
+            print(str)
+        }
+        let 吧=[1:"A",2:"C",3:"8"]
+        for( a , b ) in 吧{
+            print("\(a):\(b)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
